dcdb0a5cda5f180d1e231689b49a67962a81ae99
Kingsley Eneja <kingsley.eneja@getir.com>
2024-11-07 15:52:41 +0100

    Separate frontend from backend

--- quickpeek_frontend/index.js
+++
@@ -1,4 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App'; // Point to your main App component
-
-registerRootComponent(App);
--- quickpeek_frontend/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as NavigationBar } from './NavigationBar';
-export { default as CustomButton } from './CustomButton';
